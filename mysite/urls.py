2d9ede087468ff887f58a7aa302d4aee635331c9
Tobias Wilken <tooangel@tooangel.de>
2014-11-26 12:10:58 +0100

    Update dependencies
     - Update Django==1.7.1, gunicorn==19.1.1 and MySQL-python==1.2.5
     - Adapt Procfile to start gunicorn
     - Add gunicorn config
     - Add TEST_RUNNER due to Django 1.7.1
     - Add staticfiles urlpatterns

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.autodiscover()
 
@@ -9,3 +10,4 @@ urlpatterns = patterns(
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+urlpatterns += staticfiles_urlpatterns()
